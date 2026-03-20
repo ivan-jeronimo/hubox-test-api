fe73d50db61884351dd8445af091bb2285c6cb59
mauro <mauroivaning@gmail.com>
2026-03-19 23:10:18 -0600

    php artisan route:clear php artisan config:clear php artisan cache:clea php artisan route:clear php artisan config:clear php artisan cache:clearfix cors

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 🔥 IMPORTANTE: manejar preflight (CORS)
+Route::options('/{any}', function () {
+    return response('', 200);
+})->where('any', '.*');
